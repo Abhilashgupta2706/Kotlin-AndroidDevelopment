46896b457d836ed5b36423fb81e2b47d46f972f4
Abhilashgupta2706 <abhilash.gupta.official@gmail.com>
2022-08-05 15:20:31 +0530

    Live Data

--- ViewModelLiveData/app/src/main/java/com/example/viewmodellivedata/MainActivity.kt
+++ ViewModelLiveData/app/src/main/java/com/example/viewmodellivedata/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
@@ -18,11 +19,14 @@ class MainActivity : AppCompatActivity() {
         val tvCount = findViewById<TextView>(R.id.tvCount)
         val btnCount = findViewById<Button>(R.id.btnCount)
 
-        tvCount.text = viewModel.totalCount.toString()
+//        tvCount.text = viewModel.totalCount.toString()
+        viewModel.totalCount.observe(this, Observer {
+            tvCount.text = it.toString()
+        })
 
         btnCount.setOnClickListener {
             viewModel.increaseCount()
-            tvCount.text = viewModel.totalCount.toString()
+//            tvCount.text = viewModel.totalCount.toString()
         }
     }
 }
--- ViewModelLiveData/app/src/main/java/com/example/viewmodellivedata/MainActivityViewModel.kt
+++ ViewModelLiveData/app/src/main/java/com/example/viewmodellivedata/MainActivityViewModel.kt
@@ -1,11 +1,18 @@
 package com.example.viewmodellivedata
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel : ViewModel() {
-    var totalCount = 0
+    //    var totalCount = 0
+    var totalCount = MutableLiveData<Int>()
 
-   fun  increaseCount(){
-       ++totalCount
-   }
+    init {
+        totalCount.value = 0
+    }
+
+    fun increaseCount() {
+//       ++totalCount
+        totalCount.value = (totalCount.value)?.plus(1)
+    }
 }
