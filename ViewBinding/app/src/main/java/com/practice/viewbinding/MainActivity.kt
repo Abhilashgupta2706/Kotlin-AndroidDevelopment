7cb71e92885a45a7db9cf8c28349aa7c9e300dd3
Abhilash Gupta <88545049+Abhilashgupta2706@users.noreply.github.com>
2022-08-24 11:49:39 +0530

    View Binding - Concept

--- ViewBinding/app/src/main/java/com/practice/viewbinding/MainActivity.kt
+++ ViewBinding/app/src/main/java/com/practice/viewbinding/MainActivity.kt
@@ -2,25 +2,38 @@ package com.practice.viewbinding
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.practice.viewbinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tvMessage: TextView = findViewById(R.id.tvMessage)
-        val etName: EditText = findViewById(R.id.etName)
-        val btnSubmit: Button = findViewById(R.id.btnSubmit)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        btnSubmit.setOnClickListener {
-            val name = etName.text.toString()
-            tvMessage.text = "Hello! $name"
-            etName.setText("")
+//        val tvMessage: TextView = findViewById(R.id.tvMessage)
+//        val etName: EditText = findViewById(R.id.etName)
+//        val btnSubmit: Button = findViewById(R.id.btnSubmit)
+
+//        binding.btnSubmit.setOnClickListener {
+//            val name = binding.etName.text.toString()
+//            binding.tvMessage.text = "Hello! $name"
+//            binding.etName.setText("")
+//        }
+
+        binding.apply {
+            btnSubmit.setOnClickListener {
+                val name = etName.text.toString()
+                tvMessage.text = "Hello! $name"
+                etName.setText("")
+            }
         }
+
     }
 }
